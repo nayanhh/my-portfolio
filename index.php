5a1284c40db3e35ba8d4467d7df90d934833f664
nayanhh <nayunayana14@gmail.com>
2025-06-16 13:20:02 +0530

    Create index.html

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Portfolio</title>
+</head>
+<body>
+    <h1><?php echo "Hello, I'm Nayana!"; ?></h1>
+    <p>Welcome to my PHP portfolio site.</p>
+</body>
+</html>
